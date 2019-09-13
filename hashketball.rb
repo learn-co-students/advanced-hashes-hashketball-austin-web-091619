1cb1744f1aca8ae3211dd2f61211cfc85294bde4
Vincent Marks <violateacher123@gmail.com>
2019-09-13 23:39:54 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,52 @@
-# Write your code here!
+require "pry"
+def game_hash
+return hash = {
+  :home => {
+    :team_name => 'Brooklyn Nets ',
+    :colors => [Black, White],
+    :players => ['Alan Anderson' => 
+      {:number => '',
+      :shoe => '',
+      :points => '',
+      :rebounds => '',
+      :assists => '',
+      :steal => '',
+      :blocks => '',
+      :slam_dunks => ''}
+    ]
+  } , 
+  :away => {
+    :team_name => ' ',
+    :colors => [],
+    :players => [
+      {:number => '',
+      :shoe => '',
+      :points => '',
+      :rebounds => '',
+      :assists => '',
+      :steal => '',
+      :blocks => '',
+      :slam_dunks => ''}
+    ]
+  }
+}
+end
+ def good_practices
+   game_hash.each do |location, team_data|
+     binding.pry
+     team_data.each do |attribute, data|
+       binding.pry
+       data.each do |data_item|
+         binding.pry
+       end
+    end
+  end
+end
+good_practices
+    
+     
+
+ 
 
 
 
